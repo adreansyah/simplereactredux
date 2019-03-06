5c42987a2a7233a5e801543c4347a50461cb7665
root <adreansyah.aswafi@yahoo.com>
2019-03-06 23:52:49 +0700

    Updates

---
+++ precache-manifest.b9d835603fe3a6a129b64bc36ba228c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e612b03d1b3dfc2871f",
+    "url": "/adreansyah/simplereactredux.git/static/css/main.c4249f82.chunk.css"
+  },
+  {
+    "revision": "8e612b03d1b3dfc2871f",
+    "url": "/adreansyah/simplereactredux.git/static/js/main.8e612b03.chunk.js"
+  },
+  {
+    "revision": "4c7e57fe2e7ef57f1606",
+    "url": "/adreansyah/simplereactredux.git/static/js/runtime~main.4c7e57fe.js"
+  },
+  {
+    "revision": "18ccfff34d0db70ea889",
+    "url": "/adreansyah/simplereactredux.git/static/js/2.18ccfff3.chunk.js"
+  },
+  {
+    "revision": "dce94115fa13452603c075bd602cce24",
+    "url": "/adreansyah/simplereactredux.git/index.html"
+  }
+];
